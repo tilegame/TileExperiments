c52635d85a4a744d4f89132e5b17d5bc5717e7df
Fractalbach <fractalbach@gmail.com>
2018-08-27 03:27:50 -0700

    code cleanup; game object; camera.js
    - There is now a "game" prototype object.
    - cleaned up the ugly "Draw Sequence".
    - can more clearly see WHEN code gets executed.
    - Added camera.js for future use.

---
+++ js/camera.js
@@ -0,0 +1,22 @@
+
+// ================================================
+// Camera
+// ------------------------------------------------
+
+// The camera will remain seperate from the player's position.  In this
+// version of the game, the camera is actually determined by the player's
+// position, but that will be kept seperate from the class definitions.
+
+class Camera {
+	constructor() {
+		this.x
+		this.y
+		this.width
+		this.height
+		this.maxX
+		this.maxY
+	}
+}
+
+console.log("camera.js loaded")
+
--- js/main.js
+++ js/main.js
@@ -1,91 +1,103 @@
 // ================================================
-// Init Canvas
+// Init Game
 // ------------------------------------------------
+var game = {}
 
-let GameCanvas = document.querySelector('#GameCanvas')
-let ctx = GameCanvas.getContext('2d')
-let TILE_SIZE = 64
+// Magic Variables
+game.TILE_SIZE = 64
+
+// Include References to Canvas and Drawing Context
+game.Canvas = document.querySelector('#GameCanvas')
+game.ctx = game.Canvas.getContext('2d')
+
+// Atlas: the source image for the visual tiles
+game.Atlas = {}
+
+// LogicalMap: contains data for types of tile.
+game.LogicalMap = {}
+
+
 
 // ================================================
 // Downloading Map Data
 // ------------------------------------------------
 
-// Atlas: the source image for the visual tiles
-// LogicalMap: contains data for types of tile.
-let Atlas = {}
-let LogicalMap = {}
-
-// Define the location of the json file containing the map.
-let prefixURL = "https://fractalbach.github.io/TileExperiments/"
-let mapURL = prefixURL + "json/example.json"
-let mapURL2 = prefixURL + "tools/ex.json"
-
-
-fetch(
-	mapURL2
-).then(
-	response => response.json()
-).catch(
-	error => console.error('Fetch Error:', error)
-).then(
-	myJson => handleJson(myJson)
-)
-
 // handleJson basically just saves the json data into memory.
-function handleJson(myJson) {
-	Atlas = myJson.Atlas
-	LogicalMap = myJson.Map
+game.handleJson = (myJson) => {
 	console.log("Fetched Map:", myJson)
-	BuildMap()
+	
+	// add the JSON data to the game.
+	game.Atlas = myJson.Atlas
+	game.LogicalMap = myJson.Map
+
+	// Fetch and Load the image for the atlas.
+	game.Atlas.img = new Image()
+	game.Atlas.img.src = game.Atlas.ImagePath
+	console.log(game.Atlas.ImagePath)
+
+	// Once the image has been fetched and loaded, drawAll will be called.
+	game.Atlas.img.addEventListener('load', game.DrawMap, false)
+}
+
+game.FetchMap = function() {
+
+	// Define the location of the json file containing the map.
+	let prefix = window.location.href
+	let example1 = prefix + "json/example.json"
+	let example2 = prefix + "tools/ex.json"
+	console.log(`Fetching map from: ${example2}`)
+
+	fetch(
+		example2
+	).then(
+		response => response.json()
+	).then(
+		myJson => game.handleJson(myJson)
+	).catch(
+		error => console.error('Fetch Error:', error)
+	)
 }
 
 
+
 // ================================================
-// Build Map Sequence
+// Drawing the Map
 // ------------------------------------------------
 // Source: s
 // Destination: d
 // Position: x | y 
 // Width: w
 // Height: h
-function BuildMap() {
+game.DrawLayer = function(matrix) {
 	let sx, sy, sw, sh, dx, dy, dw, dh, val
-	sw = Atlas.TileWidth
-	sh = Atlas.TileHeight
-	dw = TILE_SIZE
-	dh = TILE_SIZE
+	sw = game.Atlas.TileWidth
+	sh = game.Atlas.TileHeight
+	dw = game.TILE_SIZE
+	dh = game.TILE_SIZE
 
-	// Load the tile atlas, which is saved in a single image file.
-	let TileAtlas = new Image()
-	TileAtlas.src = Atlas.ImagePath
-
-	// Draws the map from a Javascript array object.
-	function ArrayToTiles(matrix) {
-		for (let i = 0; i < matrix.length; i++) {
-			for (let j = 0; j < matrix[i].length; j++) {
-				val = matrix[i][j]
-				if (val <= 0) {
-					continue
-				}
-				dx = j * dw
-				dy = i * dh
-				sx = ((val-1) % Atlas.ImageCols) * sw
-				sy = div(val-1, Atlas.ImageCols) * sh
-				ctx.drawImage(TileAtlas,sx,sy,sw,sh,dx,dy,dw,dh)
+	for (let i = 0; i < matrix.length; i++) {
+		for (let j = 0; j < matrix[i].length; j++) {
+			val = matrix[i][j]
+			if (val <= 0) {
+				continue
 			}
+			dx = j * dw
+			dy = i * dh
+			sx = ((val-1) % game.Atlas.ImageCols) * sw
+			sy = div(val-1, game.Atlas.ImageCols) * sh
+			game.ctx.drawImage(game.Atlas.img,sx,sy,sw,sh,dx,dy,dw,dh)
 		}
 	}
-	function drawAll() {
-		nLayers = LogicalMap.Data.length
-		for (var i = 0; i < nLayers; i++) {
-			ArrayToTiles(LogicalMap.Data[i])
-			console.log(`layer ${i} drawn.`)
-		}
-		DebugTools.OutlineMiddleTile(GameCanvas, TILE_SIZE)
+}
+
+
+game.DrawMap = function() {
+	let nLayers = game.LogicalMap.Data.length
+	for (let i = 0; i < nLayers; i++) {
+		game.DrawLayer(game.LogicalMap.Data[i])
+		console.log(`layer ${i} drawn.`)
 	}
-
-	TileAtlas.addEventListener('load', drawAll, false)
-
+	DebugTools.OutlineMiddleTile(game.Canvas, game.TILE_SIZE)
 }
 
 
@@ -98,3 +110,14 @@ function div(a, b) {
 	return ~~((a-1) / b)
 }
 
+
+
+
+// ================================================
+// Main
+// ------------------------------------------------
+function main(event) {
+	game.FetchMap()
+}
+window.addEventListener("load", main);
+console.log("main.js loaded");
