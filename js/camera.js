2dcb0fecbc0331dbfa7772ba9faff26dd1fbc076
fractalbach <fractalbach@gmail.com>
2018-08-30 00:55:14 -0700

    added to Camera; which defines the visible tiles around locations.

--- js/camera.js
+++ js/camera.js
@@ -2,37 +2,50 @@
 // Camera
 // ------------------------------------------------
 
-// The camera will remain seperate from the player's position.  In this
-// version of the game, the camera is actually determined by the player's
-// position, but that will be kept seperate from the class definitions.
+/*
+The camera will remain seperate from the player's position.  In this
+version of the game, the camera is actually determined by the player's
+position, but that will be kept seperate from the class definitions.
+
+The Camera determines the tiles that are visible at any given point
+in time, and generates iterators for use by the Renderer.
+*/
 
 class Camera {
     constructor() {
+        // The tile where the camera is centered.
         this.position = new TileLocation(10,10)
-        this.viewport = {
-            width: game.canvas.width,
-            height: game.canvas.height
-        }
-        this.max_pixel = {
-        }
-        this.px_max = game.LogicalMap.Width * game.TILE_SIZE
-        this.py_max = game.LogicalMap.Height * game.TILE_SIZE
+
+        // The radius (in tiles) around which everything is visible.
+        // NOTE: For now, this will not be a circle radius, and instead
+        // will be the the side length of a square.
+        this.VIEW_RADIUS = 10
+
+        // Rectangle Bounds.
+        // Values used frequently by the generators, these should be 
+        // updated whenever the camera position changes.
+        this.UpdateBounds()
     }
 
+    // Update calculates the bounds based on the current camera position.
+    UpdateBounds() {
+        this.x0 = this.position.x - this.VIEW_RADIUS
+        this.xf = this.position.x + this.VIEW_RADIUS
+        this.y0 = this.position.y - this.VIEW_RADIUS
+        this.yf = this.position.y + this.VIEW_RADIUS
+    }
+
+    // VisibleTiles Generates an Iterator of TileLocations.
+    // Later on, this function will accept parameters that define the
+    // shape of the visual area (for example, a Circle instead of a Rectangle).
     *VisibleTiles() {
-        let n = 10
-        while (n > 0) {
-            n--
-            yield n
+        for (let x = this.x0; x < this.xf; x++) {
+            for (let y = this.y0; y < this.yf; y++) {
+                yield(new TileLocation(x,y))
+            }
         }
     }
 
-}
+    
 
-class TileIterator {
-    constructor(matrix) {
-        this.x = 0
-        this.y = 0
-        this.matrix = matrix
-    }
 }
--- js/tiles.js
+++ js/tiles.js
@@ -2,16 +2,26 @@
 // Tiles
 // ------------------------------------------------
 
+class Rect {
+    constructor(x, y, w, h) {
+        this.x = x
+        this.y = y
+        this.w = w
+        this.h = h
+    }
+}
+
+class Circle {
+    constructor(x, y, r) {
+        this.x = x
+        this.y = y
+        this.r = r
+    }
+}
+
 class TileLocation {
     constructor(x, y) {
         this.x = x
         this.y = y
     }
-}
-
-class VisibleTile {
-    constructor() {
-        this.list = []
-    }
-
 }
