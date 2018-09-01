dae4660889d1ecb6edb5484494dffe08ec17db92
fractalbach <fractalbach@gmail.com>
2018-09-01 06:09:09 -0700

    added game.classes.Player

--- js/lib/player.js
+++ js/lib/player.js
@@ -3,8 +3,25 @@
 // ------------------------------------------------
 
 game.player = {
-    position: {x:10, y:10},
     init() {
         this.canvas = document.querySelector("#MainCharacter")
+    },
+
+    moveTo(x,y) {
+
     }
+}
+
+{
+    class Player {
+        constructor(canvas) {
+            this.canvas = canvas
+            this.ctx = canvas.getContext('2d')
+            this.position = {x:1, y:1}
+            this.targetposition = {x:1, y:1}
+            
+        }
+    }
+
+    game.classes.Player = Player
 }
