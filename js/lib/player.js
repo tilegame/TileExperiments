939245028f8ac599a34a6a4938fca19a57acb384
fractalbach <fractalbach@gmail.com>
2018-09-03 03:21:55 -0700

    Added setPos(tx, ty) function for Player object!
    - added z-index to div wrappers to maintain correct layering
      when moving around the player object.  Seems to be working.
      Shouldn't matter what order the elements appear in the HTML,
      as long as the z-index is in the correct order.

--- js/lib/player.js
+++ js/lib/player.js
@@ -4,12 +4,10 @@
 
 game.player = {
     init() {
-        this.canvas = document.querySelector("#MainCharacter")
+        let myCanvas = document.querySelector("#mainPlay")
+        this.me = new game.classes.Player(myCanvas)
     },
 
-    moveTo(x,y) {
-
-    }
 }
 
 {
@@ -17,9 +15,28 @@ game.player = {
         constructor(canvas) {
             this.canvas = canvas
             this.ctx = canvas.getContext('2d')
-            this.position = {x:1, y:1}
-            this.targetposition = {x:1, y:1}
-            
+            this.position = {
+                tx: 1,
+                ty: 1
+            }
+            this.targetposition = {
+                tx: 1,
+                ty: 1
+            }
+
+        }
+
+        // setPos force-moves the player to the exact position specified
+        // by directly changing the position both logically and visually.
+        setPos(tx, ty) {
+            this.position = {
+                tx,
+                ty
+            }
+            let top = game.TILE_SIZE * (ty - game.camera.FirstTile.y)
+            let left = game.TILE_SIZE * (tx - game.camera.FirstTile.x)
+            this.canvas.style.setProperty('top', `${top}px`)
+            this.canvas.style.setProperty('left', `${left}px`)
         }
     }
 
