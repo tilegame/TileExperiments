7b0b9eca49d0a1d181092cb63ff28b2bf855f9ee
fractalbach <fractalbach@gmail.com>
2018-09-05 22:53:57 -0700

    fixed player styles and player creation

--- js/lib/player.js
+++ js/lib/player.js
@@ -4,11 +4,7 @@
 
 game.player = {
     init() {
-        let myCanvas = document.querySelector("#mainPlay")
-        this.me = new game.classes.Player(myCanvas)
-        this.me.name = "me"
-        this.me.currentPos = {tx:5, ty:5}
-        this.me.targetPos = {tx:5, ty:5}
+        this.me = game.classes.Player.New('me', 5, 5)
     },
 
 }
@@ -32,25 +28,27 @@ game.player = {
         // NewPlayer creates a new canvas for the player at the specified
         // location, and attaches it to the document body.  The player
         // object is returned.
-        static NewPlayer(name, tx, ty) {
+        static New(name, tx, ty) {
 
             // Creates a new Canvas.
             let canvas = document.createElement('canvas')
             let attr = {
                 width: game.TILE_SIZE,
                 height: game.TILE_SIZE,
-                top: 0,
-                left: 0,
+                class: 'player',
             }
-            for (let [key, val] of Object.entries(attr)) {
+            for (let[key,val] of Object.entries(attr)) {
                 canvas.setAttribute(key, val)
             }
             document.querySelector('#WrapPlayerLayer').appendChild(canvas)
-            
+
             // Creates the player objects and passes it the new canvas.
             let p = new Player(canvas)
             p.name = name
-            p.currentPos = {tx, ty}
+            p.currentPos = {
+                tx,
+                ty
+            }
 
             return p
         }
